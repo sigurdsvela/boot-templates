ec8430a4a7045d5babb49094d66d004ef1d90a47
sigurdsvela <sigurdbergsvela@gmail.com>
2015-04-27 15:08:28 +0200

    Implement wordpress-plugin template

---
+++ templates/wordpress-plugin/static/wp-content/plugins/[[!plugin-name]].php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: [[!plugin-name]]
+Plugin URI: http://plugin-uri.com/
+Description: description
+Version: 0.0.0
+Author: Your_Name
+Author URI: Author_URI
+License: LISENCE
+Text Domain: [[!plugin-name]]
+*/
